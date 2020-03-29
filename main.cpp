e4f62d26d5b5532fe9b2550072b6ed3c6690e0a3
WuchiOnline <eric@wuchi.online>
2020-03-28 20:06:28 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,20 +2,16 @@
 #include "SDL.h"
 
 void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel);
-void plot_pixel_ndc(SDL_Surface *surface, int x, int y, Uint32 pixel, SDL_Window *targetWindow);
-
-const int baseWidth = 640;
-const int baseHeight = 480;
 
 int main(int argc, char *argv[])
 {
 
 	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window *window = SDL_CreateWindow("Software Rasterizer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
 
-	SDL_Window *window = SDL_CreateWindow("Software Rasterizer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, baseWidth, baseHeight, SDL_WINDOW_SHOWN);
-	SDL_SetWindowResizable(window, SDL_TRUE);
 	SDL_Surface *screen = SDL_GetWindowSurface(window);
 	SDL_FillRect(screen, 0, 0);
+<<<<<<< HEAD
 	SDL_Event event;
 	bool quit = false;
 	while (quit == false)
@@ -48,11 +44,16 @@ void plot_pixel_ndc(SDL_Surface *surface, int x_ndc, int y_ndc, Uint32 pixel, SD
 
 	int width = SDL_GetWindowSurface(targetWindow)->w;
 	int height = SDL_GetWindowSurface(targetWindow)->h;
+=======
+>>>>>>> parent of c39d239... Update main.cpp
 
-	// make sure to include error check for x, y, which must be between -1 and 1, or else break;
+	// plot pixel at any location and any color.
+	// putpixel(screen, x, y, SDL_MapRGB(screen->format, r, g, b));
 
-	// convert ndc back to dc formula, derived from reversing dc to ndc formula
+	// first example: (red pixel at 100,100);
+	putpixel(screen, 100, 100, SDL_MapRGB(screen->format, 255, 0, 0));
 
+<<<<<<< HEAD
 	int x_dc = ((x_ndc + 1) / 2)*(width);
 	int y_dc = ((y_ndc + 1) / 2)*(height);
 
@@ -65,33 +66,13 @@ void plot_pixel_ndc(SDL_Surface *surface, int x_ndc, int y_ndc, Uint32 pixel, SD
 	//	yetAnotherInt = (int)(aFloat - 0.5);
 
 	Uint8 *p = (Uint8 *)surface->pixels + y_dc * surface->pitch + x_dc * bpp;
+=======
+	SDL_UpdateWindowSurface(window);
+>>>>>>> parent of c39d239... Update main.cpp
 
-	switch (bpp) {
-	case 1:
-		*p = pixel;
-		break;
+	SDL_Delay(15000);
 
-	case 2:
-		*(Uint16 *)p = pixel;
-		break;
-
-	case 3:
-		if (SDL_BYTEORDER == SDL_BIG_ENDIAN) {
-			p[0] = (pixel >> 16) & 0xff;
-			p[1] = (pixel >> 8) & 0xff;
-			p[2] = pixel & 0xff;
-		}
-		else {
-			p[0] = pixel & 0xff;
-			p[1] = (pixel >> 8) & 0xff;
-			p[2] = (pixel >> 16) & 0xff;
-		}
-		break;
-
-	case 4:
-		*(Uint32 *)p = pixel;
-		break;
-	}
+	return 0;
 }
 
 void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
